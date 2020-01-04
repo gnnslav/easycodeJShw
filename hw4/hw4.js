5062097e1836dc2ecc0912525bde01f7aceb547a
Nadiya Gizhytska <gnnslav@ukr.net>
2020-01-05 01:21:16 +0200

    finished hw4

--- hw4/hw4.js
+++ hw4/hw4.js
@@ -1,49 +1,40 @@
 const tasks = [{
-    _id: '5d2ca9e2e03d40b326596aa7',
+    _id: "5d2ca9e2e03d40b326596aa7",
     completed: true,
-    body: 'Occaecat non ea quis occaecat ad culpa amet deserunt incididunt elit fugiat pariatur. Exercitation commodo culpa in veniam proident laboris in. Excepteur cupidatat eiusmod dolor consectetur exercitation nulla aliqua veniam fugiat irure mollit. Eu dolor dolor excepteur pariatur aute do do ut pariatur consequat reprehenderit deserunt.\r\n',
-    title: '111Eu ea incididunt sunt consectetur fugiat non.',
+    body: "Occaecat non ea quis occaecat ad culpa amet deserunt incididunt elit fugiat pariatur. Exercitation commodo culpa in veniam proident laboris in. Excepteur cupidatat eiusmod dolor consectetur exercitation nulla aliqua veniam fugiat irure mollit. Eu dolor dolor excepteur pariatur aute do do ut pariatur consequat reprehenderit deserunt.\r\n",
+    title: "Eu ea incididunt sunt consectetur fugiat non."
   },
   {
-    _id: '5d2ca9e29c8a94095c1288e0',
+    _id: "5d2ca9e29c8a94095c1288e0",
     completed: false,
-    body: 'Aliquip cupidatat ex adipisicing veniam do tempor. Lorem nulla adipisicing et esse cupidatat qui deserunt in fugiat duis est qui. Est adipisicing ipsum qui cupidatat exercitation. Cupidatat aliqua deserunt id deserunt excepteur nostrud culpa eu voluptate excepteur. Cillum officia proident anim aliquip. Dolore veniam qui reprehenderit voluptate non id anim.\r\n',
-    title: 'Deserunt laborum id consectetur pariatur veniam occaecat occaecat tempor voluptate pariatur nulla reprehenderit ipsum.',
+    body: "Aliquip cupidatat ex adipisicing veniam do tempor. Lorem nulla adipisicing et esse cupidatat qui deserunt in fugiat duis est qui. Est adipisicing ipsum qui cupidatat exercitation. Cupidatat aliqua deserunt id deserunt excepteur nostrud culpa eu voluptate excepteur. Cillum officia proident anim aliquip. Dolore veniam qui reprehenderit voluptate non id anim.\r\n",
+    title: "Deserunt laborum id consectetur pariatur veniam occaecat occaecat tempor voluptate pariatur nulla reprehenderit ipsum."
   },
   {
-    _id: '5d2ca9e2e03d40b3232496aa7',
+    _id: "5d2ca9e2e03d40b3232496aa7",
     completed: true,
-    body: 'Occaecat non ea quis occaecat ad culpa amet deserunt incididunt elit fugiat pariatur. Exercitation commodo culpa in veniam proident laboris in. Excepteur cupidatat eiusmod dolor consectetur exercitation nulla aliqua veniam fugiat irure mollit. Eu dolor dolor excepteur pariatur aute do do ut pariatur consequat reprehenderit deserunt.\r\n',
-    title: '11111Eu ea incididunt sunt consectetur fugiat non.',
+    body: "Occaecat non ea quis occaecat ad culpa amet deserunt incididunt elit fugiat pariatur. Exercitation commodo culpa in veniam proident laboris in. Excepteur cupidatat eiusmod dolor consectetur exercitation nulla aliqua veniam fugiat irure mollit. Eu dolor dolor excepteur pariatur aute do do ut pariatur consequat reprehenderit deserunt.\r\n",
+    title: "Eu ea incididunt sunt consectetur fugiat non."
   },
   {
-    _id: '5d2ca9e29c8a94095564788e0',
+    _id: "5d2ca9e29c8a94095564788e0",
     completed: false,
-    body: 'Aliquip cupidatat ex adipisicing veniam do tempor. Lorem nulla adipisicing et esse cupidatat qui deserunt in fugiat duis est qui. Est adipisicing ipsum qui cupidatat exercitation. Cupidatat aliqua deserunt id deserunt excepteur nostrud culpa eu voluptate excepteur. Cillum officia proident anim aliquip. Dolore veniam qui reprehenderit voluptate non id anim.\r\n',
-    title: 'Deserunt laborum id consectetur pariatur veniam occaecat occaecat tempor voluptate pariatur nulla reprehenderit ipsum.',
-  },
+    body: "Aliquip cupidatat ex adipisicing veniam do tempor. Lorem nulla adipisicing et esse cupidatat qui deserunt in fugiat duis est qui. Est adipisicing ipsum qui cupidatat exercitation. Cupidatat aliqua deserunt id deserunt excepteur nostrud culpa eu voluptate excepteur. Cillum officia proident anim aliquip. Dolore veniam qui reprehenderit voluptate non id anim.\r\n",
+    title: "Deserunt laborum id consectetur pariatur veniam occaecat occaecat tempor voluptate pariatur nulla reprehenderit ipsum."
+  }
 ];
-
+const tasks1 = [];
 const STATE = {
   SHOW_ALL: 0,
-  SHOW_UNCOMPLETED: 1,
+  SHOW_UNCOMPLETED: 1
 };
 
 (function (arrOfTasks) {
+  let bgCol = "#007bff4f";
   const objOfTasks = arrOfTasks.reduce((accumulator, task) => {
     accumulator[task._id] = task;
     return accumulator;
   }, {});
-
-  const sortedTasks = Object.values(objOfTasks).sort(function (a, b) {
-    if (a.completed < b.completed) {
-      return -1;
-    }
-    if (a.completed > b.completed) {
-      return 1;
-    }
-  });
-
   const container = document.querySelector(".container .list-group");
   const form = document.forms.addTask;
   const inputTitle = form.elements.title;
@@ -51,24 +42,39 @@ const STATE = {
   const containerBtn = document.querySelector(".container-btn");
   const conteinerMsg = document.querySelector(".conteiner-msg");
 
-  renderAllTasks(sortedTasks);
+  renderTasks(STATE.SHOW_ALL);
   form.addEventListener("submit", onFormSubmit);
   container.addEventListener("click", onDelete);
   container.addEventListener("click", onComplete);
-  containerBtn.addEventListener('click', containerBtnHandler);
+  containerBtn.addEventListener("click", containerBtnHandler);
 
-  function renderAllTasks(state) {
+  function renderTasks(state) {
+    const taskList = Object.values(objOfTasks)
+      .sort(function (a, b) {
+        if (a.completed < b.completed) {
+          return -1;
+        }
+        if (a.completed > b.completed) {
+          return 1;
+        }
+      })
+      .filter(task => (state ? !task.completed : task));
 
-    const taskList = sortedTasks.filter((task) => (state ? !task.completed : task));
-
-    if (!taskList) {
-      const error = 'Массив с задачами пустой';
-      renderMes(error);
+    if (taskList.length === 0) {
+      conteinerMsg.innerHTML = "";
+      renderMsg("Массив с задачами пустой");
     }
-    container.innerHTML = '';
+    templateTasks(taskList);
+  }
+
+  function templateTasks(taskList) {
+    container.innerHTML = "";
     const fragment = document.createDocumentFragment();
     taskList.forEach(function (task) {
       const li = templateLi(task);
+      if (task.completed) {
+        li.style.backgroundColor = bgCol;
+      }
       fragment.appendChild(li);
     });
     container.appendChild(fragment);
@@ -79,22 +85,29 @@ const STATE = {
     title,
     body
   }) {
-    const li = document.createElement('li');
-    li.classList.add('list-group-item', 'd-flex', 'align-items-center', 'flex-wrap', 'mt-2');
-    li.setAttribute('data-task-id', _id);
-    const span = document.createElement('span');
+    const li = document.createElement("li");
+    li.classList.add(
+      "list-group-item",
+      "d-flex",
+      "align-items-center",
+      "flex-wrap",
+      "mt-2"
+    );
+    li.setAttribute("data-task-id", _id);
+
+    const span = document.createElement("span");
     span.textContent = title;
 
-    const delBtn = document.createElement('button');
-    delBtn.classList.add('btn', 'btn-danger', 'ml-auto', 'delete-btn');
-    delBtn.textContent = 'DELETE';
+    const delBtn = document.createElement("button");
+    delBtn.classList.add("btn", "btn-danger", "ml-2", "delete-btn");
+    delBtn.textContent = "DELETE";
 
-    const doneBtn = document.createElement('button');
-    doneBtn.classList.add('btn', 'btn-primary', 'ml-auto', 'complete-btn');
-    doneBtn.textContent = 'DONE';
+    const doneBtn = document.createElement("button");
+    doneBtn.classList.add("btn", "btn-primary", "ml-auto", "complete-btn");
+    doneBtn.textContent = "DONE";
 
-    const text = document.createElement('p');
-    text.classList.add('mt-2', 'w-100');
+    const text = document.createElement("p");
+    text.classList.add("mt-2", "w-100");
     text.textContent = body;
 
     li.appendChild(span);
@@ -105,15 +118,13 @@ const STATE = {
     return li;
   }
 
-  function changeColor(el) {
-    el.style.backgroundColor = 'blue';
-  }
-
-  function renderMes(text) {
+  function renderMsg(text) {
     const fragment = document.createDocumentFragment();
-    const div = document.createElement('div');
-    const span = document.createElement('span');
+    const div = document.createElement("div");
+
+    const span = document.createElement("span");
     span.textContent = text;
+
     div.appendChild(span);
     fragment.appendChild(div);
     conteinerMsg.appendChild(fragment);
@@ -125,14 +136,15 @@ const STATE = {
     const bodyValue = inputBody.value;
 
     if (!titleValue || !bodyValue) {
-      const text = 'Введите даные';
-      renderMes(text);
+      conteinerMsg.innerHTML = "";
+      const text = "Введите данные";
+      renderMsg(text);
       return;
     }
-    conteinerMsg.remove();
+    conteinerMsg.innerHTML = "";
     const task = createNewTask(titleValue, bodyValue);
     const listItem = templateLi(task);
-    container.insertAdjacentElement('afterbegin', listItem);
+    container.insertAdjacentElement("afterbegin", listItem);
     form.reset();
   }
 
@@ -141,11 +153,13 @@ const STATE = {
       title,
       body,
       completed: false,
-      _id: '1',
+      _id: Math.random()
+        .toString()
+        .replace(".", "5d2ca9e2")
     };
     objOfTasks[newTask._id] = newTask;
     return {
-      ...newTask,
+      ...newTask
     };
   }
 
@@ -154,35 +168,38 @@ const STATE = {
   }
 
   function onDelete(e) {
-    if (e.target.classList.contains('delete-btn')) {
-      const parent = e.target.closest('[data-task-id]');
+    if (e.target.classList.contains("delete-btn")) {
+      const parent = e.target.closest("[data-task-id]");
       const id = parent.dataset.taskId;
       delTask(id);
       parent.remove();
     }
   }
 
-  function onComplete(e) {
-    if (e.target.classList.contains('complete-btn')) {
-      const parent = e.target.closest('[data-task-id]');
-      changeColor(parent);
-      const id = parent.dataset.taskId;
-      completedTasks(id);
+  function completedTasks(id, el) {
+    if (!objOfTasks[id].completed) {
+      objOfTasks[id].completed = true;
+      el.style.backgroundColor = bgCol;
+    } else {
+      objOfTasks[id].completed = false;
+      el.style.backgroundColor = "";
     }
   }
 
-  function completedTasks(id) {
-    objOfTasks[id].completed = true;
+  function onComplete(e) {
+    if (e.target.classList.contains("complete-btn")) {
+      const parent = e.target.closest("[data-task-id]");
+      const id = parent.dataset.taskId;
+      completedTasks(id, parent);
+    }
   }
 
   function containerBtnHandler(e) {
-    if (e.target.classList.contains('completed-tasks')) {
-      renderAllTasks(STATE.SHOW_UNCOMPLETED);
+    if (e.target.classList.contains("all-tasks")) {
+      renderTasks(STATE.SHOW_ALL);
     }
-
-    if (e.target.classList.contains('all-tasks')) {
-      renderAllTasks(STATE.SHOW_ALL);
+    if (e.target.classList.contains("completed-tasks")) {
+      renderTasks(STATE.SHOW_UNCOMPLETED);
     }
   }
-
 })(tasks);
