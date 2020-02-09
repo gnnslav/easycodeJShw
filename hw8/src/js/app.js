53aa42f3611478dba15d7a2824c5410ccd0076ef
Nadiya Gizhytska <gnnslav@ukr.net>
2020-02-09 14:12:14 +0200

    added hw8

--- hw8/src/js/app.js
+++ hw8/src/js/app.js
@@ -1,4 +1,64 @@
-import api from './services/apiService';
+import '../css/style.css';
+import './plugins';
+import location from './store/location';
+import formUI from './views/form';
+import ticketsUI from './views/tickets';
+import currencyUI from './views/currency';
+import favoritesUI from './views/favorites';
 
-api.countries().then(res => console.log(res));
-api.cities().then(res => console.log(res));
+document.addEventListener('DOMContentLoaded', () => {
+    initApp();
+    const form = formUI.form;
+    const allTickets = ticketsUI.ticketsContainer;
+    const favoriteTickets = favoritesUI.favoriteTicketsContainer;
+
+    async function initApp() {
+        await location.init();
+        formUI.setAutocompleteDate(location.shortCitiesList);
+    }
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        onFormSubmit();
+    });
+
+    allTickets.addEventListener('click', addToFavoriteTickets);
+    favoriteTickets.addEventListener('click', deleteFavoriteTickets);
+
+    async function onFormSubmit() {
+        const origin = location.getCityCodeByKey(formUI.originValue);
+        const destination = location.getCityCodeByKey(formUI.destinationValue);
+        const depart_date = formUI.departDateValue;
+        const return_date = formUI.returnValue;
+        const currency = currencyUI.currencyValue;
+
+        console.log(origin, destination, depart_date, return_date, currency);
+        await location.fetchTickets({
+            origin,
+            destination,
+            depart_date,
+            return_date,
+            currency,
+        });
+        ticketsUI.renderTickets(location.lastSearch);
+    }
+
+    function addToFavoriteTickets(e) {
+        if (e.target.classList.contains("add-favorite")) {
+            const addFavoriteBtn = document.getElementsByClassName('add-favorite');
+            const index = Array.indexOf(addFavoriteBtn, e.target);
+            location.serializeFavoriteTickets(index);
+            favoritesUI.renderFavoriteTickets(location.favoriteTickets);
+        }
+    }
+
+    function deleteFavoriteTickets(e) {
+        if (e.target.classList.contains('delete-favorite')) {
+            const delBtn = document.getElementsByClassName('delete-favorite');
+            const index = Array.indexOf(delBtn, e.target);
+            location.deleteFavoriteTicket(index);
+            favoritesUI.renderFavoriteTickets(location.favoriteTickets);
+        }
+    }
+
+});
